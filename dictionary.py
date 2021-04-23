bfefcb9b5499dc881c9d27aac160903c13f3b592
GraceSudha-11 <80829473+GraceSudha-11@users.noreply.github.com>
2021-04-23 20:27:35 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+words ={'abase','aberration','abhor','abject','abrasive','abstract','acclimate','accentuate'}
+
+keyword = input("Enter your choice from given below :\n A.abase    B.aberration\nC.abhor    D.abject\nE.abrasive   F.abstract\nG.acclimate    H.accentuate ")
+
+if keyword == 'A':
+    print("Abase = cause to feel shame\n")
+elif keyword == 'B':
+    print("Aberration = a state or condition markedly different from the norm\n")
+elif keyword == 'C':
+    print("Abhor = find repugnant\n")
+elif keyword == 'D':
+    print("Abject = most unfortunate or miserable\n")               
+elif keyword == 'E':
+    print("Abrasive = sharply disagreeble, unpleasant or harsh\n")
+elif keyword == 'F':
+    print("Abstract = existing only in the mind \n")
+elif keyword == 'G':
+    print("Acclimate = get used to certain environment\n")
+elif keyword == 'H':
+    print("Accentuate = stress or single out as important\n")  
+else:
+    print("INVALID TYPE!")                  
